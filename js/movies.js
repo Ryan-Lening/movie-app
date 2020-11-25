baccb65857a6ed83a959e1a404a906403a50c521
Ryan Phillips <ryan.phillips.j92@gmail.com>
2020-11-25 15:31:32 -0600

    fixed search bar issue, and inital loading display of movies, Need to do loading bar/screen

--- js/movies.js
+++ js/movies.js
@@ -13,10 +13,8 @@ const movieGenre = "https://api.themoviedb.org/3/genre/movie/list?api_key="
 
 
 //****** will populate now playing movies for page loadup***
-var searchedMovies;
-
-
-function getpopMovies(string){
+let popularMovies;
+function getPopMovies(string){
     return fetch(`${nowMovie}${versionThree}&language=en-US&page=1&include_adult=false`,
         {headers: {'Authorization': 'token' + versionThree}})
         .then(res => res.json())
@@ -25,84 +23,50 @@ function getpopMovies(string){
         .catch(console.error);
 
 }
-getpopMovies().then(movies => {
-    searchedMovies = movies.results
-    console.log(searchedMovies);
-    renderMovies(searchedMovies);
+getPopMovies().then(movies => {
+    popularMovies = movies.results
+    console.log(popularMovies);
+    renderSearchedMovies(popularMovies);
 })
 
-
-//****** event listeners for the navbar ***
-// var userSelectedGenre = " ";
-// function updateMoviesNow(e) {
-//     // e.preventDefault(); // don't submit the form, we just want to update the data
-//     var selectedGenre = userSelectedGenre;
-//     var moviesToDisplay = [];
-//     movies.forEach(function(coffee) {
-//         if(selectedRoast === "all"){
-//             filteredCoffees.push(coffee)
-//         } else if (coffee.roast === selectedRoast) {
-//             filteredCoffees.push(coffee);
-//         }
-//     });
-//     tbody.innerHTML = renderCoffees(filteredCoffees);
-// }
-
-
-
-var input = '';
-    // let movie_search = document.querySelector('#searchbar');
-    // movie_search.addEventListener('click', movieSearch)
-
+//********************************************************************************
+//***************************SEARCHED CODE AND RENDERING**************************
+//********************************************************************************
 var submitBtn = document.getElementById("submit")
-console.log(submitBtn)
-var textInput = document.getElementById("searchbar")
-
-textInput.addEventListener('input', movieSearch)
 submitBtn.addEventListener("click", function(e){
     e.preventDefault();
-    console.log('i was clciked');
-    getMovies(textInput.value);
+    let searchValue = $('#searchbar').val();
+    console.log(`This is the function running when button is clicked search value is :${searchValue}`)
+    movieSearch(searchValue)
 })
-
-
-
-
-
-    // function movieSearch (){
-    //      input = textInput.value
-    //     input = input.toLowerCase();
-    //     console.log(input);
-    //     getMovies(input);
-    //
-    // }
-
-
-
-// ****** searchers movies based titles in string format.. could be used for a search bar*****
-function getMovies(input){
-    console.log(input)
-     fetch(`${url}${versionThree}&query=${input}&page=1&include_adult=false`,
+let userInput;
+function movieSearch (value){
+    userInput = value.toLowerCase();
+    console.log(`This is the function running 2nd in line :${userInput}`)
+    getSearchedMovies(userInput)
+}
+let searchedMovies;
+function getSearchedMovies(input){
+    console.log(`This is the getSearchedMovies Function looking for value :${input}`)
+        fetch(`${url}${versionThree}&query=${input}&page=1&include_adult=false`,
         {headers: {'Authorization': 'token' + versionThree}})
-        .then(res => console.log(res.json()))
+        .then(res => res.json())
+        .then(movies => {
+            searchedMovies = movies.results
+            console.log(searchedMovies);
+            renderSearchedMovies(searchedMovies);
+        })
         .catch(console.error);
+
 }
 
+//********************************************************************************
+//********************************************************************************
+//********************************************************************************
 
-getMovies(input).then(movies => {
-    searchedMovies = movies.results;
-    console.log(searchedMovies)
-    renderMovies(searchedMovies);
-});
-
-
-
-
-
-//*******creating a cards for every single movie populated
-function renderMovies (){
+function renderSearchedMovies (input){
     let html = ``;
-    for(let movie of searchedMovies){
+    for(let movie of input){
         html += `<div class='test  card'>
           <img src="${imageUrl}${movie.poster_path}" style="width: 20em">
           <h3 class='card_header'>${movie.title}</h3>
@@ -111,26 +75,7 @@ function renderMovies (){
 
         // document.write(html);
     }
-    document.getElementById('movies').insertAdjacentHTML('afterbegin', html);
+    document.getElementById('movies').innerHTML = html;
 }
-    // searchedMovies = movies.results;
-    // console.log(searchedMovies)
 
-
-// function getMovie(movies){
-//     for(let movie of movies){
-//         console.log(movie.results);
-//     }
-//     return null;
-// }
-
-
-// function searchMovies(string){
-//     const url = `https://api.themoviedb.org/3/search/movie?api_key=${versionThree}`
-//     const options = {headers: {'authorization': 'token ' + versionThree}};
-//     return fetch(url, options)
-//         .then(res => res.json())
-//         .then(dateofPushEvent)
-//         .catch(console.error);
-// }
 
